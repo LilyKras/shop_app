8375156be3b4fb08d186f89c9d33031475229c87
lilykrasnogorova <krasnogorova0304@mail.ru>
2023-01-09 16:34:28 +0300

    Start position

---
+++ lib/screens/product_detail_screen.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../providers/products.dart';
+
+class ProductDetailScreen extends StatelessWidget {
+  // final String title;
+  // final double price;
+
+  // ProductDetailScreen(this.title, this.price);
+  static const routeName = '/product-detail';
+
+  @override
+  Widget build(BuildContext context) {
+    final arguments = ModalRoute.of(context)?.settings.arguments! as Map <String, String>; 
+    final id = arguments['id'];// is the id!
+
+    final loadedProduct = Provider.of<Products>(context, listen: false).findById(id!);
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(loadedProduct.title),
+      ),
+    );
+  }
+}
